dbe0b2879a9df6e0354b34054ebbfc354c265ed3
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-08-04 12:53:45 +0530

    Login and regiteration from created

---
+++ register.php
@@ -0,0 +1,9 @@
+<?php require_once('partials/header.php') ?>
+    <title>BlogIt | Login</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <?php include('partials\navbar.php')?>
+    <h1>Hello Register!</h1>
+</body>
+</html>

148cff06b7fd63da53447c6709e571bfe7bb1360
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-08-04 12:52:32 +0530

    Navbar Added

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require_once('partials\header.php') ?>
+
+
+    <title>BlogIt</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <?php include('partials\navbar.php')?>
+    
+</body>
+</html>
---
+++ partials/navbar.php
@@ -0,0 +1,28 @@
+<!-- navbar -->
+
+<nav class="nav">
+        <div class="left-div">
+            <a href="index.php" class='icon'>
+                BLOG IT!
+            </a>
+        </div>
+        
+        <div class="right-nav">
+            <div class="user">
+                <span>John Doe</span>
+            
+            </div>
+          
+            <div class="nav-links">
+                <ul>
+                    <li>
+                    <a href="login.php">Login</a>
+                    </li>
+                    
+                    <li>
+                        <a href="register.php">Register</a>
+                    </li>
+            </ul>
+        </div>
+    </div>
+</nav>

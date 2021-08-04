03d310fa03d7d3dbbc6a11a03abaf9efc694225a
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-08-04 13:18:52 +0530

    create blog form created

---
+++ createPost.php
@@ -0,0 +1,19 @@
+<!-- <?php include "logic.php"; ?>  -->
+<?php require_once('partials\header.php') ?>
+    <title>BlogIt | Create</title>
+</head>
+<body>
+
+    <!-- Navbar -->
+    <?php include('partials\navbar.php')?>
+
+    <div class = 'create-post'>
+        <form method="POST">
+            <input type="text" name="title" class='create-post-title' placeholder="Blog Title">
+            <textarea name="content" cols="30" rows="40" placeholder="Write your blog here...."></textarea>
+            <button name = "new_post" id='add-post-btn'>Add Post</button>
+        </form>
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php require_once('partials\header.php') ?>
-
-
     <title>BlogIt</title>
 </head>
 <body>

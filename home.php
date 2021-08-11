238f33fc94a119a0b52a3d6a33c21f58d2cef1df
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-08-11 12:29:36 +0530

    Added banner

--- home.php
+++ home.php
@@ -12,6 +12,12 @@
     <?php include('partials\navbar.php')?>
     <?php include('partials\menuLinks.php')?>
 
+    <div class="home__banner">
+        <img src="https://images.unsplash.com/photo-1511649475669-e288648b2339?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80" alt="">
+        <div class="fade">
+        
+        </div>
+    </div>
     
 
     <div>

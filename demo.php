d726188769165d35eb21fcf153bdf20b4c59c2b3
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-08-17 14:06:50 +0530

    Added scss to the project directory

---
+++ demo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="static\css\main.css">
+   
+</head>
+<body>
+   
+
+    <div class="contact__container">
+        <h3>Contact Us!</h3>
+        <div class="field">
+            <input type="text" placeholder="Name">
+        </div>
+
+        <div class="field">
+            <input type="email" placeholder="Email">
+        </div>
+
+        <div class="field">
+            <input type="number" placeholder="Contact">
+        </div>
+
+        <button class = "contact__button" type = 'submit'>Submit</button>
+
+    </div>
+    
+</body>
+</html>

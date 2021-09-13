93122303f12fbeb5ac1ac76031d900cb93eeb051
RChaubey16 <ruturajchaubey16@gmail.com>
2021-09-13 15:54:55 +0530

    Gulp implementation completed

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -2,9 +2,14 @@ const gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
 // Task 1: Converting SASS to CSS
-gulp.task('sass', function(){
+gulp.task('sass', async function(){
     return gulp.src('scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('css/'));
+});
+
+// Task 2: Watch task
+gulp.task("watch", async function(){
+    gulp.watch('scss/*.scss', gulp.series('sass'));
 });
 

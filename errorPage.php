9d2d8c2df17ee97ef00a17ff119c8aaa5afeff67
RChaubey16 <ruturajchaubey16@gmail.com>
2021-09-14 17:26:04 +0530

    Created the error 404 page

--- errorPage.php
+++ errorPage.php
@@ -16,18 +16,17 @@
     <?php include('partials/navbar.php')?>
     <?php include('partials/menuLinks.php')?>
 
-    <?php 
-        $error = $_SERVER['REDIRECT_STATUS'];
 
-        if ($error == 404){ ?>
+    <h2 class = "search__heading"> 
+      <i style = "color: red;" class="fas fa-exclamation-triangle"></i>
+      404, Page not found!
+    </h2>
 
-        <h2 class = "search__heading">No Blogs related to: <span style = "color: #0488d1;"><?php echo $category; ?></span></h2>
+    <div class="search__noDataFound">
+      <img src="https://image.freepik.com/free-vector/no-data-concept-illustration_114360-616.jpg" alt="">
+    </div>
 
-        <div class="search__noDataFound">
-            <img src="https://image.freepik.com/free-vector/no-data-concept-illustration_114360-616.jpg" alt="">
-        </div>
 
-    <?php } ?>
     
 
 <?php require_once('partials/footer.php') ?>

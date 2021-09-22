09ba5a70df3dbf68c061f457f325d937d9308454
RChaubey16 <ruturajchaubey16@gmail.com>
2021-09-22 11:51:21 +0530

    Updated the hamburger menu design

--- home.php
+++ home.php
@@ -367,7 +367,10 @@
 
         <div class="about">
             <h3>About Us</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <?php 
+                $about = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+            ?>
+            <p><?php echo substr($about, 0, 170) . "..."; ?></p>
         </div>
 
         <div class="details">

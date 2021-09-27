bafb8fe9ac7de55f42b07249b6a0c14c6c7f37b9
RChaubey16 <ruturajchaubey16@gmail.com>
2021-09-27 18:23:38 +0530

    Updated the database file

--- study.php
+++ study.php
@@ -1,6 +1,6 @@
 <?php 
 
-    header("Location: http://localhost/BlogIt/search.php?searchBar=Travel", true, 302);
+    header("Location: http://localhost/BlogIt/search.php?searchBar=Travel", true, 301);
     exit;
 
 ?>

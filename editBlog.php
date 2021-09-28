ab23c44adfb8dc9b3d1519b7e3004dd5e0379baa
Libbna <libbna260296@gmail.com>
2021-09-28 22:36:59 +0530

    deleted extra comment of textarea tag

--- editBlog.php
+++ editBlog.php
@@ -22,7 +22,6 @@
                 <input type="text" hidden name = "id" value="<?php echo $q['id']?>">
                 <input type="text" name="title" class="create-post-title" placeholder="Blog Title" value = "<?php echo $q['title'] ?>">
                 <textarea name="editor1"><?php echo $q['content']; ?></textarea>
-                <!-- <textarea name="content" cols="30" rows="10"><?php echo $q['content'] ?></textarea> -->
                 <button name = "update" id='add-post-btn' style=>Update</button>
             </form>
 

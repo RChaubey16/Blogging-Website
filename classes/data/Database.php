2943336fe4f16ac9064a30f5bb9c8d5e66f7e424
RChaubey16 <ruturajchaubey16@gmail.com>
2021-10-12 16:37:01 +0530

    Few updates

---
+++ classes/data/Database.php
@@ -0,0 +1,11 @@
+<?php 
+
+    namespace blogit\Data;
+
+    class Database {
+        public function __construct() {
+            echo "Database class" . "<br>";
+        }
+    }
+
+?>
--- composer_autoload.php
+++ composer_autoload.php
@@ -1,13 +1,14 @@
 <?php 
 
+    
+    require_once realpath('vendor/autoload.php');
 
-    require 'vendor/autoload.php';
-
-    $game = new blogit\Game("IronMan");
+    $game = new blogit\Game("Cap");
     $animal = new blogit\Animal("Lion");
     $game->displayMsg();
     $animal->displayMsg();
 
+    $data = new \blogit\data\Database();
 
 
 ?>
